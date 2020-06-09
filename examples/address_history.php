df72f06156b627ea78b1aa09ac87b8a92cf7b76b
Blockmove.io <dev@blockmove.io>
2020-06-09 17:41:06 +0600

    Wallet and address history examples

---
+++ examples/address_history.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once '../src/APIClient.php';
+require_once '../src/APIException.php';
+require_once '../src/APIRequestException.php';
+
+use BlockmoveAPI\APIClient;
+use BlockmoveAPI\APIException;
+use BlockmoveAPI\APIRequestException;
+
+// Generated API Key from your Blockmove.io Wallet
+$apiKey = 'YOUR_API_KEY';
+
+// Generated API Secret Key from your Blockmove.io Wallet
+$apiSecret = 'YOUR_API_SECRET';
+
+// Wallet Address
+$address = 'ADDRESS';
+
+// Records Params
+$params = [
+	'limit' => 10,
+	'offset' => 5
+];
+
+// Token symbol for currencies like Ethereum, for example: USDC, TUSD, etc.
+$token = 'TOKEN_SYMBOL';
+
+
+try {
+	$result = APIClient::init($apiKey, $apiSecret)
+						->getAddressHistory($address, $params, $token);
+	
+	print_r($result);
+}
+catch (APIException $e) {
+	echo 'API Error: ' . $e->getMessage();
+}
+catch (APIRequestException $e) {
+	echo 'API Request failed: ' . $e->getMessage();
+}
